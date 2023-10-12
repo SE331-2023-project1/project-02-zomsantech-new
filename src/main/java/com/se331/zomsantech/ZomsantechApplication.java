e70aad8c5437d487ae1fc47ee998ec206cbce935
sorawee <veevisorawee@gmail.com>
2023-10-13 02:12:51 +0700

    initialize

---
+++ src/main/java/com/se331/zomsantech/ZomsantechApplication.java
@@ -0,0 +1,13 @@
+package com.se331.zomsantech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomsantechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomsantechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se331/zomsantech/ZomsantechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se331.zomsantech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomsantechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
