6264eec8c22e1e8d98812b5974f08c018a9065b4
Ratchaporn <hrkfan321@gmail.com>
2023-10-19 20:13:58 +0700

    Add uplaod file to VeeVi dev (Still not working tho)

---
+++ src/main/java/com/se331/zomsantech/controller/BucketController.java
@@ -0,0 +1,31 @@
+package com.se331.zomsantech.controller;
+
+import com.se331.zomsantech.util.CloudStorageHelper;
+import jakarta.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class BucketController {
+    final CloudStorageHelper cloudStorageHelper;
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<?> uploadFile(@RequestPart(value = "file")MultipartFile file)
+            throws ServletException, IOException {
+        return ResponseEntity.ok(this.cloudStorageHelper.getImageUrl(file,"se-lab-331-imageuplaod.appspot.com"));
+    }
+
+    @PostMapping("/uploadImage")
+    public ResponseEntity<?> uploadFileComponent(@RequestPart(value = "image") MultipartFile file)
+            throws IOException, ServletException {
+        return ResponseEntity.ok(this.cloudStorageHelper.getStorageFileDto(file, "se-lab-331-imageuplaod.appspot.com"));
+    }
+}
