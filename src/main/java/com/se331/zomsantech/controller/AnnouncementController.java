7c8c61e81a3803b7030c4ee489677b570cb2293c
Ratchaporn <hrkfan321@gmail.com>
2023-10-22 17:30:55 +0700

    Can get All and singular announcement in postman

--- src/main/java/com/se331/zomsantech/controller/AnnouncementController.java
+++ src/main/java/com/se331/zomsantech/controller/AnnouncementController.java
@@ -6,16 +6,21 @@ import com.se331.zomsantech.service.AnnouncementService;
 import com.se331.zomsantech.util.CloudStorageHelper;
 import com.se331.zomsantech.util.LabMapper;
 import jakarta.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @RestController
 @RequestMapping("/api/v1/announcements")
+@RequiredArgsConstructor
 public class AnnouncementController {
 
     @Autowired
@@ -26,13 +31,29 @@ public class AnnouncementController {
     @Autowired
     private AnnouncementRepository announcementRepository;
 
-    public AnnouncementController() {
-    }
 
     @PostMapping
     public ResponseEntity<AnnouncementDTO> createAnnouncement(@RequestParam("teacherId") Long teacherId, @ModelAttribute Announcement announcement, @RequestPart("files") MultipartFile files) {
         AnnouncementDTO announcementDTO = LabMapper.INSTANCE.getAnnouncementDTO(announcementService.createAnnouncement(teacherId, announcement, files));
         return ResponseEntity.ok(announcementDTO);
 
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<AnnouncementDTO>> getAllAnnouncements() {
+        List<Announcement> announcements = announcementRepository.findAll();
+        List<AnnouncementDTO> announcementDTOs = announcements.stream()
+                .map(announcement -> LabMapper.INSTANCE.getAnnouncementDTO(announcement))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(announcementDTOs);
+    }
+
+    @GetMapping("/{announcementId}")
+    public ResponseEntity<AnnouncementDTO> getSingleAnnouncement(@PathVariable Long announcementId) {
+        Optional<Announcement> announcement = announcementRepository.findById(announcementId);
+        if (announcement.isPresent()) {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getAnnouncementDTO(announcement.get()));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
